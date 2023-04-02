1beb90a4b70fd5a3f3a0362094e8e070bbfbfa86
durgesh14 <34915741+durgesh14@users.noreply.github.com>
2023-04-02 23:16:22 +0530

    added controller for hotel count

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -6,6 +6,7 @@ import {
   getHotel,
   getHotels,
   updateHotel,
+  countByCity,
 } from "../controllers/hotels.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
@@ -20,9 +21,11 @@ router.put("/:id", verifyAdmin, updateHotel);
 router.delete("/:id", verifyAdmin, deleteHotel);
 
 //GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 
 //GET ALL
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", getHotels);
 
 export default router;
